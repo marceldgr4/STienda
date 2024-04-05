b01043961a811f26c0fc0698ea58483ee65870cb
marceldgr4 <marceldgr@gmail.com>
2024-04-05 02:11:28 -0500

    Initial commit

---
+++ src/test/java/com/Unimagda/STienda/STiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Unimagda.STienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class STiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
